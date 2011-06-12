be502e976f0cf44e997f010146547f7b9cdb307e
vltsu <tsukanov.vl@gmail.com>
2011-06-12 20:04:11 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transunion::Application.config.secret_token = 'a74fa9acd5bd74e9478a676453b303398cc6f46aece8124a0dc7997b65dbee7be0c367b0366f6b0de8bb54612eb2affbfb99cd4b82cee8c17ce068748cb431b2'
