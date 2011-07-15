6b09c052ece4d2b05fc9bb5246fbc9b287b5ced4
vltsu <tsukanov.vl@gmail.com>
2011-07-15 17:11:44 +0400

    validators for requests

--- app/models/loading_point.rb
+++ app/models/loading_point.rb
@@ -3,4 +3,8 @@ class LoadingPoint < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 20
   belongs_to :request
+
+  validates  :contact_person, :presence => { :message => 'Введите контактное лицо' }
+
+
 end
